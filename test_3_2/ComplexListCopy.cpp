2ed98ad7fa87c4c7d1101fb2691580ae8866bac8
caozongkai1008 <czk_971008@163.com>
2019-03-21 23:45:31 +0800

    总结

--- test_3_2/ComplexListCopy.cpp
+++ test_3_2/ComplexListCopy.cpp
@@ -1,23 +1,34 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 typedef struct RandomListNode
 {
   int _val;
   struct RandomListNode* _next;
   struct RandomListNode* _random;
-};
+}RandomListNode;
 
 class solution
 {
   public:
+    
+    RandomListNode* BuyNode(int val)
+    {
+      RandomListNode* newNode = (RandomListNode*)malloc(sizeof(RandomListNode));
+      newNode->_next = NULL;
+      newNode->_random = NULL;
+      newNode->_val = val;
+      return newNode;
+    }
 
     RandomListNode* Clone(RandomListNode* pHead)
     {
-      if(nullptr == pHead)
+      if(NULL == pHead)
         return NULL;
       RandomListNode* pCur = pHead;
       while(pCur)
       {
-        RandomListNode* Node = pCur->_next;
+        RandomListNode* Node = BuyNode(pCur->_val);
         Node->_next = pCur->_next;
         pCur->_next = Node;
         pCur = Node->_next;
@@ -44,3 +55,28 @@ class solution
       return pCloneHead;
     }
 };
+
+//////////////////////////// 
+//第二种写法
+//////////////////////////// 
+int main()
+{
+  solution s;
+  RandomListNode* node1 = s.BuyNode(1);
+  RandomListNode* node2 = s.BuyNode(2);
+  RandomListNode* node3 = s.BuyNode(3);
+  
+  node1->_next = node2;
+  node1->_random = node3;
+  node2->_next = node3;
+  node2->_random = node2;
+  node3->_next = NULL;
+  node3->_random = node1;
+  RandomListNode* newNode = s.Clone(node1);
+  while(newNode)
+  {
+    printf("%d ",newNode->_val);
+    newNode = newNode->_next;
+  }
+  return 0;
+}
